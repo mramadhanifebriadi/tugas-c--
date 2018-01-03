fb609f153bb9c8c62030b24ef0fb28f4d472a17f
mramadhanifebriadi <32356499+mramadhanifebriadi@users.noreply.github.com>
2018-01-03 21:21:06 +0700

    DEKLARASI VARIABEL DAN ISI VARIABEL BERDASARKAN ARRAY

--- tugassaw.cpp
+++ tugassaw.cpp
@@ -7,3 +7,9 @@ int main(){
 	cout.setf(ios::fixed);
 	cout.setf(ios::showpoint);
 	cout.precision(4);
+	
+
+	int i,j,n,b,a[8];
+	double c[i][j], ac[3][5] = {{4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2}};
+	double w[] = {5,3,4,4,2};
+	double hasil1,hasil2,hasil3,hasilkali,mc1,mc2,mc3,mc4,mc5,h1,h2,h3,h4,h5,h11;
