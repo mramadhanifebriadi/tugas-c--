ffb7cad1baa9d7d477d6caa7f01d1bc1c7897821
mramadhanifebriadi <32356499+mramadhanifebriadi@users.noreply.github.com>
2018-01-03 21:21:56 +0700

    ISI DATA YANG MANUAL PENCARIAN NILAI MAX

--- tugassaw.cpp
+++ tugassaw.cpp
@@ -13,3 +13,9 @@ int main(){
 	double c[i][j], ac[3][5] = {{4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2}};
 	double w[] = {5,3,4,4,2};
 	double hasil1,hasil2,hasil3,hasilkali,mc1,mc2,mc3,mc4,mc5,h1,h2,h3,h4,h5,h11;
+	
+		mc1=max(4,max(3,5));
+		mc2=max(4,max(3,4));
+		mc3=max(5,max(4,2));
+		mc4=max(3,max(2,2));
+		mc5=max(3,max(3,2));
